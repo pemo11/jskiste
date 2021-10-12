d52a3bdbf5cdffa4c362db8ec5228555738ccae1
Peter Monadjemi <pm@activetraining.de>
2021-10-12 12:50:11 +0200

    ReaktionstesterV2

---
+++ Arrays/FindIndex.js
@@ -0,0 +1,9 @@
+// Beispiel für die FindIndex-Methode
+
+var z = [11,22,33,44,55,66,77,88,99]
+
+console.log(z.findIndex((n) => n == 11))
+
+console.log(z.findIndex((n) => n == 44))
+
+console.log(z.findIndex((n) => n == 42))
