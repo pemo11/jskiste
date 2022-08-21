0b87ce7229467c94219759246812365eccb7bb4e
Peter Monadjemi <pm@activetraining.de>
2022-08-21 15:20:23 +0200

    Weitere Beispiele (u.a. ZooKlicker und Zeitgefuehl)

--- Arrays/ElementInArray.js
+++ Arrays/ElementInArray.js
@@ -2,10 +2,12 @@
 
 var z = [11,22,33,44,55,66]
 
+// Gibt das erste Element zurück, das das Prädikat erfüllt
 var r = z.find(e => e == 22)
 
 console.log(r)
 
+// Findet nichts, so dass r am Ende undefiniert ist
 r = z.find(e => e == 10)
 
 console.log(r == undefined)
